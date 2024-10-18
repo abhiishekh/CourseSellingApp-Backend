849ef271efa4216f7baa9a669729c3c44b77c3c2
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2024-10-18 16:23:59 +0530

    Final commit

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const UserRouter = require('./routes/userRoute')
 const AdminRouter= require('./routes/adminRoute')
 const CourseRouter  = require('./routes/courseRoute')
 const path = require('path')
+require('dotenv').config()
+
+const MONGO_URL = process.env.MONGO_URL
+const PORT = process.env.PORT
 
 const app = express()
 // Connect to the database 
 
-mongoose.connect('mongodb+srv://abhishekmaurya:abhishek123@coursesellingapp.kqvuq.mongodb.net/CourseSelling')
+mongoose.connect(MONGO_URL)
 .then(()=>{
     console.log("database connected")
 }).catch(err=>{
@@ -33,6 +37,6 @@ app.use('/api/v1',CourseRouter)
 
 
 
-app.listen(3000,
-    console.log("App is listening")
+app.listen(PORT,
+    console.log(`App is listening on PORT: ${PORT}`)
 )
